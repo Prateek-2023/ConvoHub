7ad9d95651dc9d9ad1c5cad1dfe4eb39e64386ea
Prateek-2023 <prateek174503@gmail.com>
2025-09-04 19:59:55 +0530

    Updated code

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 //Function to connect to mongo db   
 export const connectDB = async()=>{
     try{
-
         mongoose.connection.on('connected',()=>console.log('Database connected'))
+        mongoose.connection.on('error',(err)=>console.error('Mongo error:', err.message))
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+        const uri = process.env.MONGODB_URI;
+        if(!uri) throw new Error('MONGODB_URI not set');
+
+        await mongoose.connect(uri, { serverSelectionTimeoutMS: 10000 });
     }catch(error){
-        console.log(error)
+        console.log('Mongo connection failed:', error.message)
+        process.exit(1);
     }
 }
